f00e934f22cf4d41e7915cc04dd45d9c1df138e6
Wender1903 <wenderman74@gmail.com>
2025-05-26 14:24:31 -0300

    :sparkles: feat: Criando Modelo para a criação de Atividades, definindo os atributos que irão compor ela, e método para exibir seus dados (menos a resposta)

--- ApiAtv/atividadesmicrosservicos/atividade_model.py
+++ ApiAtv/atividadesmicrosservicos/atividade_model.py
@@ -1,32 +1,24 @@
-atividades = [
-    {
-        'id_atividade': 1,
-        'id_disciplina': 1,
-        'enunciado': 'Crie um app de todo em Flask',
-        'respostas': [
-            {'id_aluno': 1, 'resposta': 'todo.py', 'nota': 9},
-            {'id_aluno': 2, 'resposta': 'todo.zip.rar'},
-            {'id_aluno': 4, 'resposta': 'todo.zip', 'nota': 10}
-        ]
-    },
-    {
-        'id_atividade': 2,
-        'id_disciplina': 1,
-        'enunciado': 'Crie um servidor que envia email em Flask',
-        'respostas': [
-            {'id_aluno': 4, 'resposta': 'email.zip', 'nota': 10}
-        ]
-    }
-]
+from config import db
+class Atividades(db.Model):
+    __tablename__ = 'atividade'
 
-class AtividadeNotFound(Exception):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    professor_id = db.Column(db.Integer, nullable=False)
+    enunciado = db.Column(db.String(250), nullable=False)
+    alternativas = db.Column(db.Text, nullable=False)
+    resposta = db.Column(db.String(1), nullable=False)
 
-def listar_atividades():
-    return atividades
 
-def obter_atividade(id_atividade):
-    for atividade in atividades:
-        if atividade['id_atividade'] == id_atividade:
-            return atividade
-    raise AtividadeNotFound
+    def __init__(self, professor_id, enunciado, alternativas, resposta):
+        self.professor_id = professor_id
+        self.enunciado = enunciado
+        self.alternativas = alternativas
+        self.resposta = resposta
+        
+    def exibir_atividade(self):
+        return {"id": self.id, 
+                "professor_id": self.professor_id, 
+                "enunciado": self.enunciado, 
+                "alternativas": self.alternativas.split(' | ')}
+
+    
